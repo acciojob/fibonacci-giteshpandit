af234f0d900da13602be51563661c03aaa542caf
giteshpandit <panditgitesh38@gmail.com>
2024-08-14 22:35:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,22 +6,21 @@ function fibonacci(num) {
     // Initialize the first two Fibonacci numbers
     let a = 0; // F(0)
     let b = 1; // F(1)
-    let fib = 0;
-
+    
     // Compute the Fibonacci numbers iteratively
     for (let i = 2; i <= num; i++) {
-        fib = a + b; // F(n) = F(n-1) + F(n-2)
-        a = b;      // Update F(n-2) to the previous F(n-1)
-        b = fib;    // Update F(n-1) to the current F(n)
+        let next = a + b; // Compute the next Fibonacci number
+        a = b;           // Update 'a' to the previous 'b'
+        b = next;        // Update 'b' to the newly computed Fibonacci number
     }
     
-    return fib;
+    return b; // Return the nth Fibonacci number
 }
 
 // Example usage
 console.log(fibonacci(0)); // Output: 0
 console.log(fibonacci(1)); // Output: 1
+console.log(fibonacci(2)); // Output: 1
 console.log(fibonacci(5)); // Output: 5
 console.log(fibonacci(10)); // Output: 55
-
-
+console.log(fibonacci(50)); // Output: 12586269025
