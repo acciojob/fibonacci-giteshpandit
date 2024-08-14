0b14385e3acf8fbeb14158fc3ab7bedd4ec29355
giteshpandit <panditgitesh38@gmail.com>
2024-08-14 16:41:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function fibonacci(num) {
-    if (num === 0) return 0;
-    if (num === 1) return 1;
+    if (num === 0) return 0; // The 0th Fibonacci number is 0
+    if (num === 1) return 1; // The 1st Fibonacci number is 1
 
-    let a = 0;
-    let b = 1;
+    let a = 0; // F(0)
+    let b = 1; // F(1)
     
+    // Use a loop to compute the Fibonacci number at position 'num'
     for (let i = 2; i <= num; i++) {
         let temp = a + b;
         a = b;
@@ -15,4 +16,5 @@ function fibonacci(num) {
 }
 
 module.exports = fibonacci;
+
 
